ac3f411b0dbb6457edb4b51ba8c10f9d3b9f86ee
LOREYLOLIM <emmanuellorey@gmail.com>
2022-12-18 15:43:38 +0300

    first commit

---
+++ lib/bottom_nav.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOption = <Widget>[
+    const Text('Home'),
+    const Text('Search'),
+    const Text('Ticket'),
+    const Text('Profile'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOption[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.airplane_ticket), label: "ticket"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile")
+          ]),
+    );
+  }
+}
